04251ee70858fe33278238304bee7a272fc6c233
ceejay004 <chijiokenwankwor@gmail.com>
2026-02-23 21:23:54 +0000

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,28 +1,3 @@
-export const config = { runtime: "nodejs18.x" };
-
-export default async function handler(req, res) {
-  try {
-
-    const auth = Buffer.from(process.env.EBAY_APP_ID + ":").toString("base64");
-
-    const r = await fetch("https://api.ebay.com/identity/v1/oauth2/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Basic " + auth
-      },
-      body: "grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope"
-    });
-
-    const data = await r.json();
-
-    if (data.access_token) {
-      res.json({ success: true });
-    } else {
-      res.json({ error: data });
-    }
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
+const auth = Buffer.from(
+  process.env.EBAY_APP_ID + ":" + process.env.EBAY_CERT_ID
+).toString("base64");
